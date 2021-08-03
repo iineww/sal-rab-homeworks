c0c8abbf875b9ce0f19086492cbe8cf8bbe44e92
iineww <i-new@ya.ru>
2021-08-03 21:49:37 +0400

    iшестая

--- task_3/solution.js
+++ task_3/solution.js
@@ -9,9 +9,9 @@ function renderCartItem(item) {
     // title - наименование
     // price - цена
     // count - количество
-    let itemCountText = `${product.count} x ${product.price} ₽`;
+    let itemCountText = `${product[count]} x ${product[price]} ₽`;
     let sum = product.count * product.price;
-    itemCountText = itemCountText +  `= ${sum}₽`;
+    itemCountText = itemCountText + `= ${sum}₽`;
     // сформируйте строку itemCountText вида:
     // количество × цена ₽ = стоимость ₽
     // например,
