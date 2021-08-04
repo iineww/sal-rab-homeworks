4c82e2911cfefaf841006e5098d298811847bdb2
iineww <i-new@ya.ru>
2021-08-04 23:22:57 +0400

    финал - попытка вторая

--- task_final/solution.js
+++ task_final/solution.js
@@ -40,7 +40,6 @@ function sendRequest(name, phone, address, goods, sum) {
 
     for (let i = 0; i <= countOfGoods; i += 1) {
         data.goods.push(goods[i].title);
-        data.goods.push(goods[i].count);
     }
 
     data.order.address = address;
