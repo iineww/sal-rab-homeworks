478c266a1a6fc5945e7be22d938354df89cf3cfd
iineww <i-new@ya.ru>
2021-08-07 12:56:13 +0400

    итого готово

--- task_final/solution.js
+++ task_final/solution.js
@@ -38,11 +38,11 @@ function sendRequest(name, phone, address, goods, sum) {
  
     let countOfGoods = goods.length;
     for (let i = 0; i < countOfGoods; i += 1) {
-        let goods1 = {title: "", count: ""};
-        goods1.title = goods[i].title; 
-        goods1.count = goods[i].count;
+        //let goods1 = {title: "", count: ""};
+        //goods1.title = goods[i].title; 
+        //goods1.count = goods[i].count;
         
-        data.goods.push(goods1);
+        data.goods.push({title: goods[i].title, count: goods[i].count});
     }
     data.order.address = `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв ${address.flat}`;
     data.order.sum = sum;
